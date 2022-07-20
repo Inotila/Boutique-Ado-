3ac7520d6ae63f546dacb25d94ca72da956dd505
Inotila <inoati1991@gmail.com>
2022-07-20 08:31:34 +0000

    added some more sort stuff

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,  redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from .models import Product, Category
@@ -10,38 +10,36 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
-    catergories = None
+    categories = None
     sort = None
     direction = None
 
     if request.GET:
         if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
             if 'direction' in request.GET:
-                sortkey = request.GET['sort']
-                sort = sortkey
-                if sortkey == 'name':
-                    sortkey == 'lower_name'
-                    products == products.annotate(lower_name=Lower('name'))
-                if sortkey == 'category':
-                    sortkey = 'category__name'
-                if 'direction' in request.GET:
-                    direction = request.GET['direction']
-                    if direction == 'desc':
-                        sortkey = f'-{sortkey}'
-                products = products.order_by(sortkey)
-
-
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+            
         if 'category' in request.GET:
-            catergories = request.GET['category'].split(',')
-            products = products.filter(category__name__in=catergories)
-            catergories = Category.objects.filter(name__in=catergories)
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
 
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
                 messages.error(request, "You didn't enter any search criteria!")
-                return redirect(reversed('products'))
-
+                return redirect(reverse('products'))
+            
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
@@ -50,7 +48,7 @@ def all_products(request):
     context = {
         'products': products,
         'search_term': query,
-        'current_categories': catergories,
+        'current_categories': categories,
         'current_sorting': current_sorting,
     }
 
